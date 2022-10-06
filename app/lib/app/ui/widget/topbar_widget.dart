6fe0e045f489ef9ee17163b002ab037e49e2b683
Alex Li <github@alexv525.com>
2022-10-07 01:30:36 +0800

    🐛 Check permission before entering scan

--- app/lib/app/ui/widget/topbar_widget.dart
+++ app/lib/app/ui/widget/topbar_widget.dart
@@ -6,17 +6,82 @@ import 'package:app/app/ui/widget/button_widget.dart';
 import 'package:app/app/util/platform_util.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class TopBar extends StatelessWidget {
-  bool needInfo;
-  bool needScan;
-  bool needBack;
+  const TopBar({
+    super.key,
+    this.needInfo = false,
+    this.needScan = false,
+    this.needBack = false,
+  });
 
-  TopBar(
-      {super.key,
-      this.needInfo = false,
-      this.needScan = false,
-      this.needBack = false});
+  final bool needInfo;
+  final bool needScan;
+  final bool needBack;
+
+  void onInfoIconClick() {
+    Get.bottomSheet(
+      Container(
+        width: double.infinity,
+        height: 560,
+        decoration: BoxDecoration(
+          color: appThemeData.scaffoldBackgroundColor,
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 34, top: 34, right: 34, bottom: 50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  R.assetsImagesAppIconMid,
+                  width: 139,
+                  height: 39,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  'Description text ' * 10,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: ColorStyle.color_black_60,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 27),
+                child: Text(
+                  'Contact us',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: ColorStyle.color_black_60,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  // todo: 分享
+                ],
+              ),
+              const Spacer(),
+              Button(
+                width: double.infinity,
+                height: 61,
+                onPressed: () => Get.back(),
+                data: 'OK',
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,82 +120,16 @@ class TopBar extends StatelessWidget {
           ),
           if (needScan)
             GestureDetector(
-              child: Image.asset(
-                R.assetsImagesScan,
-                width: 28,
-                height: 28,
-              ),
-              onTap: () => Get.toNamed(Routes.scanPage),
+              child: Image.asset(R.assetsImagesScan, width: 28, height: 28),
+              onTap: () async {
+                final status = await Permission.camera.request();
+                if (status.isGranted) {
+                  Get.toNamed(Routes.scanPage);
+                }
+              },
             ),
-          if (!needScan)
-            const SizedBox(
-              width: 28,
-              height: 28,
-            ),
+          if (!needScan) const SizedBox(width: 28, height: 28),
         ],
-      ),
-    );
-  }
-
-  onInfoIconClick() {
-    Get.bottomSheet(
-      Container(
-        width: double.infinity,
-        height: 560,
-        decoration: BoxDecoration(
-          color: appThemeData.scaffoldBackgroundColor,
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
-        ),
-        child: Padding(
-          padding:
-              const EdgeInsets.only(left: 34, top: 34, right: 34, bottom: 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: Image.asset(
-                  R.assetsImagesAppIconMid,
-                  width: 139,
-                  height: 39,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Text(
-                  'Description text Description text Description text Description text Description text Description text Description text ',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: ColorStyle.color_black_60,
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 27),
-                child: Text(
-                  'Contact us',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: ColorStyle.color_black_60,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  // todo: 分享
-                ],
-              ),
-              const Spacer(),
-              Button(
-                width: double.infinity,
-                height: 61,
-                onPressed: () => Get.back(),
-                data: 'OK',
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
