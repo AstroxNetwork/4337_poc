ea287a3fb6fd70e6c37a8de7b320bed23002321c
skyh24 <skyhighfeng@gmail.com>
2022-10-06 10:25:26 +0800

    feat: guardian

--- app/test/web3dart/4337tests/request_test.dart
+++ app/test/web3dart/4337tests/request_test.dart
@@ -6,20 +6,23 @@ import 'package:app/eip4337lib/utils/helper.dart';
 import 'package:web3dart/web3dart.dart';
 
 void main() async {
-  // var params = Map();
-  // params['email'] = 'skyhighfeng@gmail.com';
+  var params = Map();
+  params['email'] = 'skyhighfeng@gmail.com';
   // final response = await Request.verifyEmail(params);
-  // // params['code'] = '198GFG';
-  // // final response = await Request.addAccount(params);
-  // // {"code":400,"msg":"Code is not valid.","data":{}}
-  // // {"code":200,"msg":"Add record successfully.","data":{"jwtToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNreWhpZ2hmZW5nQGdtYWlsLmNvbSIsImlhdCI6MTY2NDQ1ODgzOSwiZXhwIjoxNjY1NzU0ODM5fQ.QssiBtGElqIwuzSIaZJyRW8Jyw_iNQmDFQaEOdm2Bmg"}}
-  // print(response.body);
+  // params['code'] = '888888';
+  // final response = await Request.addAccount(params);
+  params['wallet_address'] = '0x8af8c26D62954B5CA17B7EEA5231b0F9893aDD9f';
+  params['key'] = '0x8af8c26D62954B5CA17B7EEA5231b0F9893aDD9f';
+  final response = await Request.updateAccount(params);
+  // {"code":400,"msg":"Code is not valid.","data":{}}
+  // {"code":200,"msg":"Add record successfully.","data":{"jwtToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNreWhpZ2hmZW5nQGdtYWlsLmNvbSIsImlhdCI6MTY2NDQ1ODgzOSwiZXhwIjoxNjY1NzU0ODM5fQ.QssiBtGElqIwuzSIaZJyRW8Jyw_iNQmDFQaEOdm2Bmg"}}
+  print(response.body);
 
-  final web3 = Web3Helper.web3();
-  WalletContext.createAccount(web3);
-  final ctx = WalletContext.getInstance();
-  ctx.setWalletAddress('0x62d00c12903832be82Fb6140A35E102Cab2dE7dD');
-  // final balance = await ctx.getEthBalance();
-  final balance = await ctx.getWEthBalance();
-  print(balance);
+  // final web3 = Web3Helper.web3();
+  // WalletContext.createAccount(web3);
+  // final ctx = WalletContext.getInstance();
+  // ctx.setWalletAddress('0x62d00c12903832be82Fb6140A35E102Cab2dE7dD');
+  // // final balance = await ctx.getEthBalance();
+  // final balance = await ctx.getWEthBalance();
+  // print(balance);
 }
