169444942f0f10d97a90af6865feae0ec50abd97
Alex Li <github@alexv525.com>
2022-10-07 02:19:15 +0800

    🔧 Configure bundle to `com.astrox.soul_wallet`

--- app/android/app/src/main/kotlin/com/astrox/soul_wallet/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/astrox/soul_wallet/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.app
+package com.astrox.soul_wallet
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+  // The main activity for Flutter.
 }
