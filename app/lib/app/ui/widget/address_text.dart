d85a257bc8f08350c0d3ca010e7638aa5424a3fa
Alex Li <github@alexv525.com>
2022-10-09 01:13:04 +0800

    🚸 Long press address text will show dialog

--- app/lib/app/ui/widget/address_text.dart
+++ app/lib/app/ui/widget/address_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AddressText extends StatelessWidget {
   const AddressText(this.address, {this.style, super.key});
@@ -6,14 +7,38 @@ class AddressText extends StatelessWidget {
   final String address;
   final TextStyle? style;
 
+  Widget _buildSelectableDialog(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        child: FittedBox(
+          fit: BoxFit.cover,
+          child: SelectableText(
+            address,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      address.length > 8
-          ? '${address.substring(0, 4)}...'
-              '${address.substring(address.length - 4, address.length)}'
-          : address,
-      style: style,
+    return GestureDetector(
+      onLongPress: () => Get.dialog(_buildSelectableDialog(context)),
+      child: Text(
+        address.length > 8
+            ? '${address.substring(0, 4)}...'
+                '${address.substring(address.length - 4, address.length)}'
+            : address,
+        style: style,
+      ),
     );
   }
 }
