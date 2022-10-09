df4eba561f36c4e6ed4f4a6e2a03609ccd9334fb
Alex Li <github@alexv525.com>
2022-10-09 13:37:31 +0800

    ⚡️ Persist token from the authenticator

--- app/lib/net/intercept.dart
+++ app/lib/net/intercept.dart
@@ -1,22 +1,42 @@
+import 'dart:convert';
+
 import 'package:app/constant.dart';
 import 'package:app/eip4337lib/utils/log_util.dart';
 import 'package:dio/dio.dart';
-import 'package:get/get.dart';
+import 'package:get/get.dart' hide Response;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthInterceptor extends Interceptor {
+  SharedPreferences get _sp => Get.find<SharedPreferences>();
+
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    final String? accessToken = Get.find<SharedPreferences>().getString(
-      Constant.accessToken,
-    );
-    LogUtil.d(
-      'AuthInterceptor accessToken = $accessToken',
-      tag: 'AuthInterceptor',
-    );
+    final String? accessToken = _sp.getString(Constant.accessToken);
+    LogUtil.d('`accessToken`: $accessToken', tag: 'AuthInterceptor');
     if (accessToken != null) {
       options.headers[Constant.accessToken] = 'bearer $accessToken';
     }
     super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    final data = response.data;
+    if (data is String) {
+      try {
+        _handleMapData(jsonDecode(data));
+      } catch (_) {}
+    } else if (data is Map) {
+      _handleMapData(data);
+    }
+    super.onResponse(response, handler);
+  }
+
+  void _handleMapData(Map data) {
+    final jwtToken = data['data']?[Constant.jwtToken];
+    if (jwtToken is String && jwtToken.isNotEmpty) {
+      LogUtil.w('Setting `accessToken`: $jwtToken', tag: 'AuthInterceptor');
+      _sp.setString(Constant.accessToken, jwtToken);
+    }
   }
 }
